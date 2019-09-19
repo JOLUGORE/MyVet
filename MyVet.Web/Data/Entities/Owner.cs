9044e91fc391f7c570dc643cb6d74fa418602a15
JOLUGORE <46359913+JOLUGORE@users.noreply.github.com>
2019-09-19 00:02:23 -0500

    Create database and Add Owners Controllers

---
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public int Document { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "First Name")]
+        public int FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Last Name")]
+        public int LastName { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "Fixed Phone")]
+        public int FixedPhone { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [Display(Name = "Cell Phone")]
+        public int CellPhone { get; set; }
+
+        [MaxLength(100)]
+        public int Address { get; set; }
+
+        [Display(Name = "Owner")]
+        public string FullName => $"{FirstName} {LastName}";
+
+    }
+}
